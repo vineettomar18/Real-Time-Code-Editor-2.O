f6bc153c27f99163c601321250e20a8001b60847
vineettomar18 <taumarvineet@gmail.com>
2026-03-07 22:28:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -81,7 +81,7 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
